6afa4f595777afde73468121c61ace0d2787129a
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-08-08 16:26:48 +0900

    경로 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,5 +24,5 @@ fi
 
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
+echo "> DEPLOY_JAR 배포" >> /home/ubuntu/action/deploy.log
 sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
